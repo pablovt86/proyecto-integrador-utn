35722b502d97505e06e286e63809fed224359e3a
Delfina Mariana Cardozo <delfinac821@alu.bue.edu.ar>
2023-04-17 19:34:00 -0300

    ddfdf

--- js/consumoApi.js
+++ js/consumoApi.js
@@ -6,117 +6,32 @@ var ul  = document.getElementById('ul');
  let url = 'https://apiv2.allsportsapi.com/football/?met=Countries&APIkey=90ddcb1f66801f15c397799aa8e2119409a4f687b5ac720c933a5c6bb2e1b231';
 const api = new XMLHttpRequest();
 
-// api.open('GET', url ,true);
-// api.send();
-// api.onreadystatechange = function(){
-//   if(this.status == 200 && this.readyState == 4){
-//   datos = JSON.parse(this.responseText);
-// let paises = datos.result.slice(0,10);
-//    paises.forEach(function(pais) {
-//     let li = document.createElement('li');
-//     let a = document.createElement('a');
-//      li.textContent = pais.country_name ;
-//      a.setAttribute("href",pais.country_logo)
-//      a.appendChild(li)
-//     fragment.appendChild(a);
+api.open('GET', url ,true);
+api.send();
+api.onreadystatechange = function(){
+  if(this.status == 200 && this.readyState == 4){
+  datos = JSON.parse(this.responseText);
+let paises = datos.result.slice(0,10);
+   paises.forEach(function(pais) {
+    let li = document.createElement('li');
+    let a = document.createElement('a');
+     li.textContent = "visualize la bandera del"+ "  " + pais.country_name ;
+     a.setAttribute("href",pais.country_logo)
+     a.appendChild(li)
+    fragment.appendChild(a);
 
-// });
+});
 
-// ul.appendChild(fragment);
+ul.appendChild(fragment);
 
-// }
-// }
+}
+}
 
 
 // wizard inicio -----------------
-// /jQuery time
-// var current_fs, next_fs, previous_fs; 
-// var left, opacity, scale; //propiedades de conjunto de campos que animaremos
-// var animating; //bandera para evitar fallas rápidas de múltiples clics
-
-// $(".next").click(function(){
-// 	if(animating) return false;
-// 	animating = true;
-	
-// 	current_fs = $(this).parent();
-// 	next_fs = $(this).parent().next();
-	
-//   //activa el siguiente paso en la barra de progreso usando el índice
-// 	$("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
-	
-// //muestra el siguiente conjunto de campos
-//   next_fs.show(); 
-
-// // ocultar el conjunto de campos actual con estilo
-//   current_fs.animate({opacity: 0}, {
-// 		step: function(now, mx) {
-// 		//como la opacidad de current_fs se reduce a 0 - almacenado en "ahora"
-// //1. escalar current_fs hasta 80%
-
-
-// 			scale = 1 - (1 - now) * 0.2;
-//       //2. traer next_fs desde la derecha (50%)
-// 			left = (now * 50)+"%";
-// //3. aumentar la opacidad de next_fs a 1 a medida que avanza
-//       opacity = 1 - now;
-// 			current_fs.css({
-//         'transform': 'scale('+scale+')',
-//         'position': 'absolute'
-//       });
-// 			next_fs.css({'left': left, 'opacity': opacity});
-// 		}, 
-// 		duration: 800, 
-// 		complete: function(){
-// 			current_fs.hide();
-// 			animating = false;
-// 		}, 
-
-// // esto proviene del complemento de aceleración personalizado
-//     easing: 'easeInOutBack'
-// 	});
-// });
-
-// $(".previous").click(function(){
-// 	if(animating) return false;
-// 	animating = true;
-	
-// 	current_fs = $(this).parent();
-// 	previous_fs = $(this).parent().prev();
-	
-// //desactivar el paso actual en la barra de progreso
-//   $("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
-	
-// //muestra el conjunto de campos anterior
-// 	previous_fs.show(); 
-// // ocultar el conjunto de campos actual con estilo
-//   current_fs.animate({opacity: 0}, {
-// 		step: function(now, mx) {
-// 			//como la opacidad de current_fs se reduce a 0 - almacenado en "ahora"
-//     //1. escalar previous_fs de 80% a 100%
-// 			scale = 0.8 + (1 - now) * 0.2;
-//       //2. tome current_fs a la derecha (50%) - desde 0%
-// 			left = ((1-now) * 50)+"%";
-// //3. aumentar la opacidad de previous_fs a 1 a medida que avanza
-// 			opacity = 1 - now;
-// 			current_fs.css({'left': left});
-// 			previous_fs.css({'transform': 'scale('+scale+')', 'opacity': opacity});
-// 		}, 
-// 		duration: 800, 
-// 		complete: function(){
-// 			current_fs.hide();
-// 			animating = false;
-// 		}, 
-//     // esto proviene del complemento de aceleración personalizado
-
-//     easing: 'easeInOutBack'
-// 	});
-// });
-
-
-//jQuery time
-var current_fs, next_fs, previous_fs; //fieldsets
-var left, opacity, scale; //fieldset properties which we will animate
-var animating; //flag to prevent quick multi-click glitches
+var current_fs, next_fs, previous_fs; 
+var left, opacity, scale; //propiedades de conjunto de campos que animaremos
+var animating; //bandera para evitar fallas rápidas de múltiples clics
 
 $(".next").click(function(){
 	if(animating) return false;
@@ -125,21 +40,24 @@ $(".next").click(function(){
 	current_fs = $(this).parent();
 	next_fs = $(this).parent().next();
 	
-	//activate next step on progressbar using the index of next_fs
+  //activa el siguiente paso en la barra de progreso usando el índice
 	$("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
 	
-	//show the next fieldset
-	next_fs.show(); 
-	//hide the current fieldset with style
-	current_fs.animate({opacity: 0}, {
+//muestra el siguiente conjunto de campos
+  next_fs.show(); 
+
+// ocultar el conjunto de campos actual con estilo
+  current_fs.animate({opacity: 0}, {
 		step: function(now, mx) {
-			//as the opacity of current_fs reduces to 0 - stored in "now"
-			//1. scale current_fs down to 80%
+		//como la opacidad de current_fs se reduce a 0 - almacenado en "ahora"
+//1. escalar current_fs hasta 80%
+
+
 			scale = 1 - (1 - now) * 0.2;
-			//2. bring next_fs from the right(50%)
+      //2. traer next_fs desde la derecha (50%)
 			left = (now * 50)+"%";
-			//3. increase opacity of next_fs to 1 as it moves in
-			opacity = 1 - now;
+//3. aumentar la opacidad de next_fs a 1 a medida que avanza
+      opacity = 1 - now;
 			current_fs.css({
         'transform': 'scale('+scale+')',
         'position': 'absolute'
@@ -151,8 +69,9 @@ $(".next").click(function(){
 			current_fs.hide();
 			animating = false;
 		}, 
-		//this comes from the custom easing plugin
-		easing: 'easeInOutBack'
+
+// esto proviene del complemento de aceleración personalizado
+    easing: 'easeInOutBack'
 	});
 });
 
@@ -163,20 +82,20 @@ $(".previous").click(function(){
 	current_fs = $(this).parent();
 	previous_fs = $(this).parent().prev();
 	
-	//de-activate current step on progressbar
-	$("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
+//desactivar el paso actual en la barra de progreso
+  $("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
 	
-	//show the previous fieldset
+//muestra el conjunto de campos anterior
 	previous_fs.show(); 
-	//hide the current fieldset with style
-	current_fs.animate({opacity: 0}, {
+// ocultar el conjunto de campos actual con estilo
+  current_fs.animate({opacity: 0}, {
 		step: function(now, mx) {
-			//as the opacity of current_fs reduces to 0 - stored in "now"
-			//1. scale previous_fs from 80% to 100%
+			//como la opacidad de current_fs se reduce a 0 - almacenado en "ahora"
+    //1. escalar previous_fs de 80% a 100%
 			scale = 0.8 + (1 - now) * 0.2;
-			//2. take current_fs to the right(50%) - from 0%
+      //2. tome current_fs a la derecha (50%) - desde 0%
 			left = ((1-now) * 50)+"%";
-			//3. increase opacity of previous_fs to 1 as it moves in
+//3. aumentar la opacidad de previous_fs a 1 a medida que avanza
 			opacity = 1 - now;
 			current_fs.css({'left': left});
 			previous_fs.css({'transform': 'scale('+scale+')', 'opacity': opacity});
@@ -186,11 +105,11 @@ $(".previous").click(function(){
 			current_fs.hide();
 			animating = false;
 		}, 
-		//this comes from the custom easing plugin
-		easing: 'easeInOutBack'
+    // esto proviene del complemento de aceleración personalizado
+
+    easing: 'easeInOutBack'
 	});
 });
-
 
 
 
